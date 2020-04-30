3720fe0822da8b1bbd3feb1c8dd36fefdca80565
Lenovo <olegfom97@mail.ru>
2020-04-30 20:45:36 +0300

    minor changes

--- src/main/java/com/example/task/manager/model/User.java
+++ src/main/java/com/example/task/manager/model/User.java
@@ -21,21 +21,6 @@ public class User implements UserDetails {
     private Role role;
     private boolean active;
 
-//    public User (Long id, String login, String password, Role role, boolean active) {
-//        this.id = id;
-//        this.username = login;
-//        this.password = password;
-//        this.role = role;
-//        this.active = active;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return String.format(
-//                "User[id=%d, login='%s', password='%s', role='%s', active='%s']",
-//                id, username, password, role.toString(), active);
-//    }
-
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         ArrayList<Role> roles = new ArrayList<>();
