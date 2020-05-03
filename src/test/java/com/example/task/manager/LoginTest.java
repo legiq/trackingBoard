24bab9ca508d9bea34db6266da89b50a00271d30
Lenovo <olegfom97@mail.ru>
2020-05-04 00:07:58 +0300

    added integration tests

---
+++ src/test/java/com/example/task/manager/LoginTest.java
@@ -0,0 +1,46 @@
+package com.example.task.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LoginTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void accessDeniedTest() throws Exception {
+        this.mockMvc.perform(get("/main"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("http://localhost/login"));
+    }
+
+    @Test
+    public void correctAuthenticationTest() throws Exception {
+        this.mockMvc.perform(formLogin().user("dev").password("1"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+    }
+
+    @Test
+    public void badCredentials() throws Exception {
+        this.mockMvc.perform(post("/login").param("user", "Adolf"))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+    }
+
+}
---
+++ src/test/java/com/example/task/manager/MainControllerTest.java
@@ -0,0 +1,32 @@
+package com.example.task.manager;
+
+import com.example.task.manager.controller.MainController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@WithUserDetails("dev")
+public class MainControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private MainController mainController;
+
+    @Test
+    public void mainPageTest() throws Exception {
+        this.mockMvc.perform(get("/main"))
+                .andDo(print())
+                .andExpect(authenticated());
+    }
+}
